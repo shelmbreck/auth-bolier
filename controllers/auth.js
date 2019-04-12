901afe00424204e48dac60c3a59b1d81e1a79759
shelmbreck <samantha.helmbreck@gmail.com>
2019-04-12 11:59:41 -0700

    installed and required flash

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,10 +6,17 @@ let router = express.Router()
 
 // Declare routes
 router.get('/login', (req, res) => {
-  res.send('Login Page Stub')
+  res.render('auth/login')
 })
 
+router.post('/login', (req, res) => {
+  res.send('Reached the route POST to /auth/login')
+})
 router.get('/signup', (req, res) => {
+  res.render('Signup Page Stub')
+})
+
+router.post('/signup', (req, res) => {
   res.send('Signup Page Stub')
 })
 
